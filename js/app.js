4631b4d32aebd39eefa39088f6b893842cbcd0bf
Stevanato10 <kauanstevanato07@gmail.com>
2024-10-24 01:03:37 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ let listaSorteio = document.getElementById('lista-sorteio');
 let amigos = [];
 
 function adicionar() {
+    verificarCampo(amigo)
+    verificarAmigoEstaNaLista(amigo)
+
     if (lista.textContent == ''){
         amigos.push(amigo.value);
         lista.textContent = amigo.value;
@@ -13,6 +16,7 @@ function adicionar() {
         lista.textContent = lista.textContent + ',' + amigo.value;     
         amigo.value = '';
     }
+    
 }
 
 function reiniciar() {
@@ -23,6 +27,10 @@ function reiniciar() {
 
 function sortear(){
     embaralha(lista)
+    if(amigos.length <= 2){
+        alert('Você precisa de mais amigos')
+        return
+    }
     for(let i = 0; i < amigos.length; i++){
         if (i == amigos.length - 1) {
             listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i] + '-->' + amigos[0] + '<br>';
@@ -37,5 +45,20 @@ function embaralha(lista) {
         const indiceAleatorio = Math.floor(Math.random() * indice);
         [lista[indice - 1], lista[indiceAleatorio]] = 
             [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function verificarCampo(amigo){
+    if(amigo.value == ''){
+        alert('O campo não pode estar vazio')
+        return
+    }
+    console.log(amigos.includes(amigo))
+}
+
+function verificarAmigoEstaNaLista(amigo){
+    if(amigos.includes(amigo.value)){
+        alert('este amigo já foi adicionado')
+        return
     }
 }
